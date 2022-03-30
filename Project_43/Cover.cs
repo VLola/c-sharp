98d2531967924cf4fefab8e63fbb3ca1f9125dca
Valentin <valiklolanew@gmail.com>
2022-03-30 15:41:47 +0300

    Add project 43

---
+++ Project_43/Cover.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_43
+{
+    class Cover
+    {
+        public PictureBox box;
+        public int NewCover(Control control, int temp)
+        {
+            if (temp < 530)
+            {
+                NewBox();
+                box.Location = new Point(temp, 16 + 176 - box.Image.Height);
+                control.Controls.Add(box);
+                temp += box.Image.Width;
+                return temp;
+            }
+            else if (temp < 1000)
+            {
+                NewBox();
+                box.Location = new Point(temp - 480, 16 + 377 - box.Image.Height);
+                control.Controls.Add(box);
+                temp += box.Image.Width;
+                return temp;
+            }
+            else if (temp < 1450)
+            {
+                NewBox();
+                box.Location = new Point(temp - 950, 16 + 570 - box.Image.Height);
+                control.Controls.Add(box);
+                temp += box.Image.Width;
+                return temp;
+            }
+            else { 
+                MessageBox.Show("Shelf full!");
+                return temp;
+            }
+        }
+        public void NewBox()
+        {
+            box = new PictureBox();
+            box.Image = NewImage();
+            box.Size = new Size(box.Image.Width, box.Image.Height);
+        }
+        public static Image NewImage()
+        {
+            if (Rnd() == 1) return Properties.Resources.cover1;
+            else if (Rnd() == 2) return Properties.Resources.cover2;
+            else if (Rnd() == 3) return Properties.Resources.cover3;
+            else return Properties.Resources.cover4;
+        }
+        public static int Rnd()
+        {
+            return new Random().Next(1, 5);
+        }
+    }
+}
---
+++ Project_43/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_43
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = $"x = {Cursor.Position.X} + y = {Cursor.Position.Y}";
+        }
+        public int temp = 50;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cover cover = new Cover();
+            temp = cover.NewCover(this, temp);
+        }
+        
+    }
+}
